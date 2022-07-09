c42c6ff9d3eb85b5dea262c7249a9c5064575b9a
Paul Kim <pbkim@usc.edu>
2022-07-08 17:44:53 -1000

    Final Push!

--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -1,6 +1,7 @@
 from battery.battery import Battery
 from utils import add_years_to_date
 
+
 class SpindlerBattery(Battery):
     def __init__(self, last_service_date, current_date):
         self.last_service_date = last_service_date
