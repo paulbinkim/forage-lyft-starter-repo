2205b8fbb6deb57425cf37cec2a449ac4be87f5e
Paul Kim <pbkim@usc.edu>
2022-07-08 19:25:26 -1000

    Push

--- test/test_battery/test_spindler_battery.py
+++ test/test_battery/test_spindler_battery.py
@@ -9,7 +9,7 @@ class TestSpindlerBattery(unittest.TestCase):
         current_date = date.fromisoformat("2020-05-15")
         last_service_date = date.fromisoformat("2017-01-25")
         battery = SpindlerBattery(current_date, last_service_date)
-        self.assertTrue(battery.needs_service())
+        self.assertFalse(battery.needs_service())
 
     def test_needs_service_false(self):
         current_date = date.fromisoformat("2020-05-15")
